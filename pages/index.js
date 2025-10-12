937f2d0a30287f73594d13af10b4348bc9eceb8f
wesleywisch <wesley_wisch@hotmail.com>
2025-10-12 12:17:54 -0300

    Configurando a parte de estilização do projeto

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,3 @@
-
-
-export default function Home(){
-  return (
-    <h1>Hello world, iniciando o TabNews</h1>
-  )
+export default function Home() {
+  return <h1>Hello world, iniciando o TabNews</h1>;
 }
