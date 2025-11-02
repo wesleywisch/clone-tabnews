5944da257ccf04ca606351022a44f2b36511a012
wesleywisch <wesley_wisch@hotmail.com>
2025-11-02 17:02:54 -0300

    feat: add support for PATCH on /api/v1/users/[username]

--- pages/api/v1/users/[username]/index.js
+++ pages/api/v1/users/[username]/index.js
@@ -4,7 +4,7 @@ import user from "models/user.js";
 
 const router = createRouter();
 
-router.get(getHandler);
+router.get(getHandler).patch(patchHandler);
 
 export default router.handler(controller.errorHandlers);
 
@@ -12,4 +12,13 @@ async function getHandler(req, res) {
   const username = req.query.username;
   const userFound = await user.findOneByUsername(username);
   return res.status(200).json(userFound);
+}
+
+async function patchHandler(req, res) {
+  const username = req.query.username;
+  const userInputValues = req.body;
+
+  const updatedUser = await user.update(username, userInputValues);
+
+  return res.status(200).json(updatedUser);
 }
