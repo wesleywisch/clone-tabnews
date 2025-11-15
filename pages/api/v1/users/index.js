1839d71d8bfa4c3822bfb87a716b4f01fd821e6a
wesleywisch <wesley_wisch@hotmail.com>
2025-11-15 15:49:10 -0300

    feat: send activation email after user registration

--- pages/api/v1/users/index.js
+++ pages/api/v1/users/index.js
@@ -1,6 +1,7 @@
 import { createRouter } from "next-connect";
 import controller from "infra/controller";
 import user from "models/user.js";
+import activation from "models/activation";
 
 const router = createRouter();
 
@@ -11,5 +12,9 @@ export default router.handler(controller.errorHandlers);
 async function postHandler(req, res) {
   const userInputValues = req.body;
   const newUser = await user.create(userInputValues);
+
+  const activationToken = await activation.create(newUser.id);
+  await activation.sendEmailToUser(newUser, activationToken);
+
   return res.status(201).json(newUser);
 }
